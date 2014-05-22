c1718c9d2f429c1beea74e14e220e6092640ffae
martincz <martincz@mokeedev.com>
2014-05-22 23:06:30 +0800

    mokee bringup
    Change-Id: I5c39efadce663f41f4eacb406f719b9a5b8f5512

--- mokee.mk
+++ mokee.mk
@@ -1,5 +1,5 @@
 # Specify phone tech before including full_phone
-$(call inherit-product, vendor/cm/config/cdma.mk)
+$(call inherit-product, vendor/mk/config/cdma.mk)
 
 # Release name
 PRODUCT_RELEASE_NAME := r950
@@ -8,15 +8,15 @@ PRODUCT_RELEASE_NAME := r950
 TARGET_SCREEN_HEIGHT := 1280
 TARGET_SCREEN_WIDTH := 720
 
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Inherit some common MK stuff.
+$(call inherit-product, vendor/mk/config/common_full_phone.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/samsung/r950/full_r950.mk)
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := r950
-PRODUCT_NAME := cm_r950
+PRODUCT_NAME := mk_r950
 PRODUCT_BRAND := samsung
 PRODUCT_MODEL := SCH-R950
 PRODUCT_MANUFACTURER := samsung
